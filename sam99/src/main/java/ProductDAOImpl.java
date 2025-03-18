3707f3cd32321154a1247b5cada58cc34c63803c
lllllls189 <lllllls189@gmail.com>
2025-03-18 17:47:56 +0900

    d

--- sam99/src/main/java/ProductDAOImpl.java
+++ sam99/src/main/java/ProductDAOImpl.java
@@ -2,6 +2,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class ProductDAOImpl implements ProductDAO {
 
@@ -50,6 +54,83 @@ public class ProductDAOImpl implements ProductDAO {
 			DBUtil.close(rst, stmt, conn);
 		}
 		return dto;
+	}
+
+	@Override
+	public List<ProductDTO> selectAll() throws SQLException {
+		ProductDTO dto = null;
+		List<ProductDTO> list = new ArrayList<>();
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		String sql = "SELECT * FROM products;";
+		ResultSet rst = null;
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			rst = stmt.executeQuery();
+			
+			while(rst.next()) {
+				dto = new ProductDTO();
+				dto.setProductId(rst.getInt(1));
+				dto.setName(rst.getString(2));
+				dto.setCategory(rst.getString(3));
+				dto.setBrand(rst.getString(4));
+				dto.setPrice(rst.getInt(5));
+				dto.setStockQuantity(rst.getInt(6));
+				list.add(dto);
+			}
+		}finally {
+			DBUtil.close(rst,stmt,conn);
+			
+		}
+		return list;
+	}
+
+	@Override
+	public int deleteProduct(int id) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		int size;
+		String sql = "DELETE FROM products"
+				+ " WHERE product_id = ? ;";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			size = stmt.executeUpdate();
+		}finally {
+			 DBUtil.close(stmt,conn);
+		}
+		return size;
+	}
+
+	@Override
+	public int updateProduct(ProductDTO dto) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		int size;
+		String sql = "UPDATE products "
+				+ "SET	name = ? , "
+				+ "	category = ? , "
+				+ "    brand = ? , "
+				+ "    price = ? , "
+				+ "    stock_quantity = ? "
+				+ "WHERE	product_id= ? ;";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, dto.getName());
+			stmt.setString(2, dto.getCategory());
+			stmt.setString(3, dto.getBrand());
+			stmt.setInt(4, dto.getPrice());
+			stmt.setInt(5, dto.getStockQuantity());
+			stmt.setInt(6, dto.getProductId());
+			size = stmt.executeUpdate();
+			
+		}finally {
+			DBUtil.close(stmt,conn);
+		}
+		return size;
 	}
 
 }
--- sam99/src/main/java/ShopServlet.java
+++ sam99/src/main/java/ShopServlet.java
@@ -1,12 +1,14 @@
 
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.SQLException;
 
 /**
  * Servlet implementation class ShopServlet
@@ -28,7 +30,21 @@ public class ShopServlet extends HttpServlet {
 		case "selectid":
 			selectProduct(request,response);
 			break;
+			
+		case "selectall":
+			selectAllProduct(request,response);
+			break;
+			
+		case "deleteid":
+			deleteProduct(request,response);
+			break;
+			
+		case "updateid":
+			updateProduct(request,response);
+			break;
 		}
+		
+
 	}
 
 	/**
@@ -40,6 +56,20 @@ public class ShopServlet extends HttpServlet {
 	}
 
 	ProductDAO dao = new ProductDAOImpl();
+	
+	protected void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		try {
+			int result = dao.deleteProduct(Integer.parseInt(id));
+			System.out.println("성공");
+			System.out.println("삭제"+ result);
+		} catch (SQLException e) {
+			System.out.println("실패");
+			System.out.println(e);
+		}
+		
+	}
 	
 	protected void selectProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
@@ -54,9 +84,51 @@ public class ShopServlet extends HttpServlet {
 		
 	}
 	
+	protected void selectAllProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			List<ProductDTO> list = dao.selectAll();
+			System.out.println("성공");
+			for(int i=0; i<list.size(); i++) {
+				System.out.println(list.get(i).toString());
+			}
+		} catch (SQLException e) {
+			System.out.println("실패");
+			System.out.println(e);
+		}
+		
+	}
+	
+	protected void updateProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductDTO dto = new ProductDTO();
+		String name = request.getParameter("name");
+		String category = request.getParameter("category");
+		String brand = request.getParameter("brand");
+		int price = Integer.parseInt(request.getParameter("price"));
+		int stockQuantity = Integer.parseInt(request.getParameter("stockquantity"));
+		int id = Integer.parseInt(request.getParameter("id"));
+	
+		dto.setName(name);
+		dto.setCategory(category);
+		dto.setPrice(price);
+		dto.setBrand(brand);
+		dto.setStockQuantity(stockQuantity);
+		dto.setProductId(id);
+		
+		try {
+			int size = dao.updateProduct(dto);
+			System.out.println("성공");
+			System.out.println(size);
+		} catch (SQLException e) {
+			System.out.println("실패");
+			System.out.println(e);
+		}
+		
+	}
+	
 	protected void writeProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ProductDTO dto = new ProductDTO();
-		
+
 		dto.setName("컴퓨터");
 		dto.setCategory("가전");
 		dto.setPrice(3000);
